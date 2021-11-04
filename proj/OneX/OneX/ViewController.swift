530b1376bbc75f286ea84e7e2a784895d2133550
邓江洲 <dengjiangzhouReal@gmail.com>
2021-11-04 17:46:51 +0800

    // 影响线程调度的因素，包含：
    // 优先级

--- proj/OneX/OneX/ViewController.swift
+++ proj/OneX/OneX/ViewController.swift
@@ -20,13 +20,17 @@ class ViewController: UIViewController {
     
     
     func testQueue(){
-        
-        queue.maxConcurrentOperationCount = 1
+        queue.maxConcurrentOperationCount = 10
+        //  queue.maxConcurrentOperationCount = 1
         for i in 0...20{
             let blockOp = BlockOperation {
                 print(i)
             }
-            if i % 2 == 0{
+            
+            //  if i % 2 == 1{
+            //  此时， 第一个调度的任务，先走
+            
+            if i % 2 == 1{
                 blockOp.queuePriority = .high
             }
             else{
@@ -41,3 +45,8 @@ class ViewController: UIViewController {
 
 }
 
+
+
+// 影响线程调度的因素，包含：
+
+// 优先级
